24f88edf5b75158aec4544cd7b6365629d2d33ba
Unknown <claesrundquist@yahoo.se>
2018-03-21 14:39:21 +0100

    Auto stash before merge of "master" and "MakeReservation_Flight"

---
+++ src/se/lexicon/model/test/TicketTest.java
@@ -0,0 +1,20 @@
+package se.lexicon.model.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import se.lexicon.model.Ticket;
+import se.lexicon.model.TicketType;
+
+public class TicketTest {
+
+	@Test
+	public void test() {
+
+		Ticket t=new Ticket(1,234,5,TicketType.BUSINESS,false);
+	
+		assertEquals("Ticket [customerID=1, flightID=234, seatNumber=5, ticketType=BUSINESS, payed=false, ticketPrice=20000]", t.toString());
+	}
+
+}
