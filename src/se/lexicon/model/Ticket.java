58ba740207aa0e21c4f89c06863bf975eabe4206
ClaesRundquist <claesrundquist@yahoo.se>
2018-03-21 10:41:07 +0100

    add class Ticket, update FlightInterface

---
+++ src/se/lexicon/model/Ticket.java
@@ -0,0 +1,65 @@
+package se.lexicon.model;
+
+public abstract class Ticket {
+
+	private int customerID;
+	private int flightID;
+	private int seatNumber;
+	private TicketType ticketType;
+	private boolean payed;
+	private int ticketPrice;
+	
+	
+
+	public Ticket(int customerID, int flightID, int seatNumber, TicketType ticketType, boolean payed, int ticketPrice) {
+		super();
+		this.customerID = customerID;
+		this.flightID = flightID;
+		this.seatNumber = seatNumber;
+		this.ticketType = ticketType;
+		this.payed = payed;
+		this.ticketPrice = ticketPrice;
+		
+		if (ticketType==TicketType.ECONOMY) {
+			this.ticketPrice=5000;
+		} else {	
+			this.ticketPrice=20000;
+		}
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	
+	public int getFlightID() {
+		return flightID;
+	}
+
+	
+	public int getSeatNumber() {
+		return seatNumber;
+	}
+
+	
+	public TicketType getTicketType() {
+		return ticketType;
+	}
+
+	public boolean isPayed() {
+		return payed;
+	}
+
+	public void setPayed(boolean payed) {
+		this.payed = payed;
+	}
+
+
+	public int getTicketPrice() {
+		return ticketPrice;
+	}
+
+	
+	
+	
+}
